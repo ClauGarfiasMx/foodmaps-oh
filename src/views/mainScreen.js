edec7602db4d8e4f386b323c78b99c87fdb00ab0
ClauGarfiasMx <apps@claudiagarfias.works>
2019-05-29 20:15:39 -0500

    map functioning but it does not filter yet, it doesn't find button on DOM

--- src/views/mainScreen.js
+++ src/views/mainScreen.js
@@ -1,33 +1,25 @@
 const mainScreen = `
-  <section class="main-screen">
-    <div class="logo-container flex-column">
-      <img src="https://claudiagarfias.works/laboratoria/sharedimages/foodmapslogo.png" />
-      <h1>find & eat</h1>
-      <p>¿qué se te antoja hoy?</p>
-    </div>
-
-    <p class="orange-p" for="myLocation-lat">Restaurantes más cercanos:</p>
-    <div class="map-container">
-      <section id="map" />
-    </div>
-    <hr>
-
-
-    <div
-    class="settings">
-
-    <p class="orange-p" for="myLocation-lat">O encuentra tu restaurante:</p>
-    <input type="text" id="myLocation-lat" class="form-control" placeholder="ALGUN TEXTO" />
-
-    <button type="submit" id="boton">BUSCAR</button>
-
-
-    <hr>
-    <h3>Resultado de Busqueda</h3>
-
-    <div id="nearbyResults">
-      <ul></ul>
-    </div>
+<section class="main-screen">
+  <div class="logo-container flex-column">
+    <img src="https://claudiagarfias.works/laboratoria/sharedimages/foodmapslogo.png" />
+    <h1>find & eat</h1>
+    <p>¿qué se te antoja hoy?</p>
   </div>
-
-  </section>`;
+  <p class="orange-p" for="myLocation-lat">Restaurantes más cercanos:</p>
+  <div class="map-container">
+    <section id="map" />
+  </div>
+  <hr>
+  <div
+  class="settings">
+  <p class="orange-p" for="myLocation-lat">O encuentra tu restaurante:</p>
+  <input type="text" id="myLocation-lat" class="form-control" placeholder="ALGUN TEXTO" />
+  <button type="submit" id="boton">BUSCAR</button>
+  <hr>
+  <h3>Resultado de Busqueda</h3>
+  <div id="nearbyResults">
+    <ul></ul>
+  </div>
+</div>
+</section>  
+`;
